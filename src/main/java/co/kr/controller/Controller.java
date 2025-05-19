9b794b1b7239fae239682d1387f79917ea8e635e
bee9827 <ehfrhfo9494@naver.com>
2025-05-19 21:29:34 +0900

    refactor: 변수명 변경
    입력에 대한 핸들링

--- src/main/java/co/kr/controller/Controller.java
+++ src/main/java/co/kr/controller/Controller.java
@@ -4,8 +4,8 @@ import co.kr.model.Menu;
 import co.kr.model.MenuService;
 import co.kr.model.RecommendTime;
 import co.kr.model.Weather;
-import co.kr.view.InputView;
 import co.kr.view.InputOption;
+import co.kr.view.InputView;
 import co.kr.view.OutputView;
 
 import java.util.HashMap;
@@ -15,29 +15,30 @@ public class Controller {
     private final InputView inputView;
     private final OutputView outputView;
     private final MenuService menuService;
-    Map<InputOption, Runnable> functions;
+    Map<InputOption, Runnable> inputHandler;
 
     public Controller(InputView inputView, OutputView outputView, MenuService menuService) {
         this.inputView = inputView;
         this.outputView = outputView;
         this.menuService = menuService;
 
-        functions = getFunctions();
+        inputHandler = getInputHandler();
     }
 
-    private Map<InputOption, Runnable> getFunctions() {
-        Map<InputOption, Runnable> functions = new HashMap<>();
-        functions.put(InputOption.GET_RECOMMEND, this::getRecommendMenu);
-        functions.put(InputOption.GET_ALL_MENUS, this::printAllMenus);
+    private Map<InputOption, Runnable> getInputHandler() {
+        Map<InputOption, Runnable> handler = new HashMap<>();
+        handler.put(InputOption.GET_RECOMMEND, this::getRecommendMenu);
+        handler.put(InputOption.GET_ALL_MENUS, this::printAllMenus);
 
-        return functions;
+        return handler;
     }
 
     public void start() {
         InputOption selected;
+        //첫 입력이 잘못되면 다시 실행하지 않는다.
         while ((selected = inputView.getOption()) != InputOption.EXIT) {
             try {
-                functions.get(selected).run();
+                inputHandler.get(selected).run();
             } catch (RuntimeException e) {
                 outputView.printError(e);
             }
